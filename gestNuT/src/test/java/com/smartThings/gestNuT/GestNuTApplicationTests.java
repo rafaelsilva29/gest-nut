275eb7349a942a20197236e534d3bec2de8c7ac0
Rafael Silva <silvarafael029@gmail.com>
2019-09-13 22:39:12 +0100

    Begin project

---
+++ gestNuT/src/test/java/com/smartThings/gestNuT/GestNuTApplicationTests.java
@@ -0,0 +1,16 @@
+package com.smartThings.gestNuT;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GestNuTApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
