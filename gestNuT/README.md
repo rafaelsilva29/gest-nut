0a16caa6241669172d34c7eaec5ac42c920f5ef1
Rafael Silva <silvarafael029@gmail.com>
2019-09-23 10:27:22 +0100

    Add config email

--- gestNuT/README.md
+++ gestNuT/README.md
@@ -1,1 +1,3 @@
 ## SmartThings : gestNuT
+
+A WebSite to order Nut, using Java spring boot and thymeleafs templates.
--- gestNuT/src/main/java/smartThings/gestNuT/config/JpaConfig.java
+++ gestNuT/src/main/java/smartThings/gestNuT/config/JpaConfig.java
@@ -60,8 +60,8 @@ public class JpaConfig {
 
     Properties additionalProperties() {
         Properties properties = new Properties();
-        properties.setProperty("hibernate.hbm2ddl.auto", "create-drop");
+        properties.setProperty("hibernate.hbm2ddl.auto", "update");
         properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
         return properties;
-}
+    }
 }
